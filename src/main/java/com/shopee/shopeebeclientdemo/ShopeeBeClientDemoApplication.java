4cf5edaf696537997b4b80d6996044c5b30d8987
quannguyen1999 <nguyendanganhquan99@gmail.com>
2023-11-28 16:06:07 +0700

    'init'

---
+++ src/main/java/com/shopee/shopeebeclientdemo/ShopeeBeClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shopee.shopeebeclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopeeBeClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopeeBeClientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopee/shopeebeclientdemo/ShopeeBeClientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopee.shopeebeclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeBeClientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
